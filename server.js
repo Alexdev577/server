11d27a96307a6d05e9c132fa02d5621ba512ec4a
adifkhan <pkadif@gmail.com>
2024-06-01 20:11:21 +0600

    june 1 update

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const server = http.createServer(app);
 
 // server running at 8000
 server.listen(8080, () => {
-  console.log(`Server Is running at 8080`);
+  console.log(`Server Is running at 8000`);
 });
