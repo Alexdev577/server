860dd574003661eeeac17a60e7f235c2c3ad5478
adifkhan <pkadif@gmail.com>
2024-06-09 18:49:20 +0600

    check

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 // listen to server on available port
 server.listen(PORT, () => {
