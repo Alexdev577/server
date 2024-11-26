df7d17bc8031bfda4c3b0c0ed60e989e80b31422
Alexdev577 <86576017+Alexdev577@users.noreply.github.com>
2024-11-26 19:16:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const corsOptions = {
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect("mongodb+srv://doadmin:28i5CB64HLQ73z0k@db-mongodb-sgp1-05239-0193f6f9.mongo.ondigitalocean.com/affburg?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-05239", {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
 });
