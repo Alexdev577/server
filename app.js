2877149d88de68b5fb10f2e9778c57dbadf82f65
Alexdev577 <86576017+Alexdev577@users.noreply.github.com>
2024-11-26 15:26:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const corsOptions = {
 
 app.use(cors());
 app.use(express.json());
-const MONGODB_URI = "mongodb+srv://doadmin:28i5CB64HLQ73z0k@db-mongodb-sgp1-05239-0193f6f9.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-05239";
+const MONGODB_URI = "mongodb+srv://doadmin:28i5CB64HLQ73z0k@db-mongodb-sgp1-05239-0193f6f9.mongo.ondigitalocean.com/affburg?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-05239";
 mongoose.connect(MONGODB_URI, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
